be91a5bfb2d26c046455ef149d9212fcb83688c8
Christian Kim <chris.kim320@gmail.com>
2025-04-25 22:43:21 -0400

    Initial Commit

---
+++ PickupApp/PickupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickupAppApp.swift
+//  PickupApp
+//
+//  Created by Christian Kim on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PickupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
